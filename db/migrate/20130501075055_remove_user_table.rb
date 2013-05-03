084b6f131d3304ef6c093a9bafefff07b786d774
David Xu <dvdyxu@gmail.com>
2013-05-02 17:23:26 -0700

    remove unneeded migration

--- db/migrate/20130501075055_remove_user_table.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserTable < ActiveRecord::Migration
-  def up
-  	drop_table :users
-    
-  end
-
-  def down
-  end
-end
--- db/migrate/20130502053318_drop_old_identification.rb
+++
@@ -1,8 +0,0 @@
-class DropOldIdentification < ActiveRecord::Migration
-  def up
-  	drop_table :identifications
-  end
-
-  def down
-  end
-end
